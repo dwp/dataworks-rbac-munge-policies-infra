3ab23a4aec4c78c09710b7148c82b8a531b5cd4d
sumit.bharadia <sumit.bharadia@engineering.digital.dwp.gov.uk>
2022-07-11 06:45:34 +0100

    update docker tag to test

--- variables.tf
+++ variables.tf
@@ -13,7 +13,7 @@ variable "image_version" {
   description = "Container tag values."
   default = {
     rbac-munge-policies = {
-      development = "debug_5"
+      development = "debug_6"
       qa          = "debug"
       integration = "debug"
       preprod     = "debug"
