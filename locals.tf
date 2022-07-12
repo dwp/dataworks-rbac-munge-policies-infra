82b20743b0375bbd7e8a9f71b76cf6634aa59c61
sumit.bharadia <sumit.bharadia@engineering.digital.dwp.gov.uk>
2022-07-12 14:39:47 +0100

    points image source to mgt and updates the tag

--- locals.tf
+++ locals.tf
@@ -8,7 +8,7 @@ locals {
 
   internal_compute_vpc_prefix_list_ids_s3 = data.terraform_remote_state.internal_compute.outputs.vpc.vpc.prefix_list_ids.s3
 
-  rbac_munge_policies_image            = "${local.account.management-dev}.${data.terraform_remote_state.aws_ingestion.outputs.vpc.vpc.ecr_dkr_domain_name}/dataworks-rbac-munge-policies:${var.image_version.rbac-munge-policies[local.environment]}"
+  rbac_munge_policies_image            = "${local.account.management}.${data.terraform_remote_state.aws_ingestion.outputs.vpc.vpc.ecr_dkr_domain_name}/dataworks-rbac-munge-policies:${var.image_version.rbac-munge-policies[local.environment]}"
   rbac_munge_policies_application_name = "rbac-munge-policies"
 
   cognito_user_pool_id = data.terraform_remote_state.cognito.outputs.cognito.user_pool_id
